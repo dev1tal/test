1aa544ecc220dc3d8d485bf91a280a477848f2e5
Sergey <dev1tal@mail.ru>
2019-10-11 10:37:09 +0300

    Это мой первый коммит

---
+++ script.js
@@ -0,0 +1,37 @@
+'use stcrict'
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2])
+
+// alert("Hello, world");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer)
+
+// let answer = prompt("Есть ли Вам 18?", "Да");
+
+// console.log(typeof(answer))
+
+// console.log("arr" + "- object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(-- decr);
+
+let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked || !isClose);
